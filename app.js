aa9777d582f82101611950f50dce602ac158ee37
Stef <sleaks@student.neumont.edu>
2018-05-31 15:20:01 -0600

    Express setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var expressSession = require('express-session');
 
 var app = express();
 
@@ -8,8 +9,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 var router = require('./src/routes/routes');
 app.use("/", router);
-var mhRoutes = require('./src/routes/mhRoutes');
-app.use("/mh/", mhRoutes);
+var userRoutes = require('./src/routes/userRoutes');
+app.use("/user/", userRoutes);
 var adminRoutes = require('./src/routes/adminRoutes');
 app.use("/admin/", adminRoutes);
 
@@ -17,6 +18,32 @@ app.use(express.static(__dirname + "/public"));
 
 app.set('views', './src/views');
 app.set('view engine', 'pug');
+
+app.use(expressSession({
+    secret: '600dm0rn1n6',
+    saveUninitialized: true,
+    resave: true
+}));
+
+var urlencodedParser = bodyParse.urlencoded({extend:false});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+//TODO: Add route for user-only pages
+
+app.get('/logout', function(req, res){
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+//TODO: Add POST for user authentication
 
 app.listen(3000, function(){
     console.log("Express Listening on Port: 3000");
