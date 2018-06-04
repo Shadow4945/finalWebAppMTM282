7e694cb52da94373094d1e09a6dc08b77c2c2c77
Stef <sleaks@student.neumont.edu>
2018-06-04 11:29:57 -0600

    Reordered login info, added check for user in database

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -23,14 +23,14 @@ var nav = [{
 {
     "name": "About",
     "path": "/about"
-},{
+}, {
     "name": "Monsters",
     "path": "/mh/monsters"
-},{
+}, {
     "name": "Add Monster",
     "path": "/mh/addMonster"
 }
-,{
+    , {
     "name": "Log Out",
     "path": "/logout"
 }
@@ -39,8 +39,8 @@ var nav = [{
 router.route("/").get(
     function (req, res) {
 
-        (async function mongo(){
-            try{
+        (async function mongo() {
+            try {
                 var client = await mongoClient.connect(url);
 
                 var db = client.db(databaseName);
@@ -50,28 +50,72 @@ router.route("/").get(
                 console.log(posts);
                 var data = {
                     title: "Threads",
-                    navOptions : nav,
+                    navOptions: nav,
                     threads: posts
                 };
                 res.render("index", data);
-            }catch(err){
+            } catch (err) {
                 res.send(err);
-            }finally{
+            } finally {
                 client.close();
             }
         }());
     }
 );
 
-router.route("/about").get(
+router.route("/login").get(
     function (req, res) {
         var model = {
-            title: "About My Monster Hunter Demo",
-            navOptions : nav
+            title: "Log In"
+            //add nav for user
         };
 
-        res.render("about", model);
+        res.render("login", model);
     }
 );
+
+router.route("/login").post(
+    function (req, res) {
+        (async function mongo() {
+            try {
+                var client = await mongoClient.connect(url);
+                var db = client.db(databaseName);
+                var user = await db.collection("users").findOne({ "username": req.body.username });
+
+                if (!user) {
+                    res.redirect("/login");
+                }
+
+                var validLogin = user.password == req.body.pass;
+
+                if (validLogin) {
+                    req.session.user = {
+                        isAuthenticated: true,
+                        username: req.body.username,
+                        isAdmin: user.type.includes("admin")
+                    };
+
+                    res.redirect("/");
+                }
+            } catch (err) {
+                console.log(err);
+            } finally {
+                client.close();
+            }
+        }());
+    }
+);
+
+router.route("/logout").get(
+    function (req, res) {
+        req.session.destroy(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect('/');
+            }
+        });
+    }
+)
 
 module.exports = router;
